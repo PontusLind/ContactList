9ed4076d87f9ef882dc55ae8d5278298f6214bfd
ACADEMY-7115VKX\Administrator <lilldeddan@gmail.com>
2017-06-28 16:31:02 +0200

    wtf

--- Contact.aspx.cs
+++ Contact.aspx.cs
@@ -10,23 +10,61 @@ using Newtonsoft.Json;
 public partial class Contact : System.Web.UI.Page
 {
     public List<Person> contactList = new List<Person>();
-    static string connectionString = "Data Source=localhost;Initial Catalog=Elsa;Integrated Security=True";
+    static string connectionString = "Data Source=localhost;Initial Catalog=Pluto;Integrated Security=True";
 
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (Page.Request["deleta"] != null)
+        if (Page.Request["ID"] != null)
         {
             //http://localhost:51616/Contact.aspx?deleta=IAMERROR
-            string input = Page.Request["deleta"];
+            string input = Page.Request["ID"];
+            GetContact(input);
         }
-        GetContacts();
-
-        myLiteral.Text = "Hello World";
+        else
+        {
+            GetContacts();
+        }
         myLiteral.Text = JsonConvert.SerializeObject(contactList);
 
     }
+    private void GetContact(string ID)
+    {
+        SqlConnection myConnection = new SqlConnection();
 
+        myConnection.ConnectionString = connectionString;
+
+        try
+        {
+            myConnection.Open();
+
+            SqlCommand myCommand = new SqlCommand();
+
+            myCommand.Connection = myConnection;
+
+            myCommand.CommandText = "select * from Contact where ID = " + ID;
+
+            SqlDataReader myReader = myCommand.ExecuteReader();
+
+            while (myReader.Read())
+            {
+                int id = Convert.ToInt32(myReader["ID"].ToString());
+                string firstName = myReader["Firstname"].ToString();
+                string lastName = myReader["Lastname"].ToString();
+
+                contactList.Add(new Person(id, firstName, lastName));
+            }
+
+        }
+        catch (Exception)
+        {
+        }
+        finally
+        {
+            myConnection.Close();
+
+        }
+    }
     private void GetContacts()
     {
         SqlConnection myConnection = new SqlConnection();
--- JavaScript.js
+++ JavaScript.js
@@ -1,15 +1,37 @@
-﻿function GetSomeJson() {
-    $.getJSON("http://localhost:51616/contact.aspx")
+﻿function GetContacts() {
+    $.getJSON("http://localhost:56993/contact.aspx")
         .done(function (data) {
-        $("#myTableBody").children().remove();
+            $("#myTableBody").children().remove();
 
-        var newTbody = "";
+            var newTbody = "";
 
-        for (var i = 0; i < data.length; i++) {
-            newTbody += "<tr><td>" + data[i].FirstName + "</td>";
-            newTbody += "<td>" + data[i].LastName + "</td>";
-            newTbody += "<td>" + data[i].ID + "</td></tr>";
-        }
-        $("#myTableBody").append(newTbody);
-    });     
+            for (var i = 0; i < data.length; i++) {
+                newTbody += "<tr><td><button onclick='UpdateContact(" + data[i].ID + ")' type='button'>" + data[i].FirstName + "</button></td>";
+                newTbody += "<td>" + data[i].LastName + "</td>";
+                newTbody += "<td>" + data[i].ID + "</td>";
+                newTbody += "<td>" + "</td></tr>";
+            }
+            $("#myTableBody").append(newTbody);
+        });
+}
+function UpdateContact(ID) {
+    $.getJSON("http://localhost:56993/contact.aspx?ID=" + ID)
+        .done(function (data) {
+            $("#myTableBody").children().remove();
+
+            var newTbody = "";
+
+            for (var i = 0; i < data.length; i++) {
+                newTbody += "<tr><td>" + data[i].FirstName + "</td>";
+                newTbody += "<td>" + data[i].LastName + "</td>";
+                newTbody += "<td>" + data[i].ID + "</td><td>";
+               
+
+                //for (var j = 0; j < data[i].Adresses.length; j++) {
+                //    myTR += data[i].Adresses[j].Type + ": " + data[i].Adresses[j].Street + "<br\>";
+                //}
+                newTbody += "</td></tr>";
+                $("#myTableBody").append(newTbody);
+            }
+        });
 }
